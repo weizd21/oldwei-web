e2d0aa24ff495a0a63b04473d1ad1027cf0240cc
weizd21 <17554157104@163.com>
2020-03-31 18:27:06 +0800

    初始化前端

---
+++ src/views/fileupload/big-file.vue
@@ -0,0 +1,76 @@
+<template>
+  <uploader :options="options" @file-error="onFileError" @file-success="onFileSuccess" @file-added="onFileAdded" @file-progress="onFileProgress" class="uploader-example">
+    <uploader-unsupport></uploader-unsupport>
+    <uploader-drop>
+      <p>拖拽文件(夹)</p>
+      <uploader-btn>多文件</uploader-btn>
+      <uploader-btn :attrs="attrs">选择图片</uploader-btn>
+      <uploader-btn :directory="true">选择文件夹</uploader-btn>
+    </uploader-drop>
+    <uploader-list></uploader-list>
+  </uploader>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        upload_key: new Date(),
+        options: {
+          // https://github.com/simple-uploader/Uploader/tree/develop/samples/Node.js
+          target: 'http://localhost:8081/chunk/upload',
+          testChunks: false
+        },
+        attrs: {
+          accept: 'image/*'
+        }
+      }
+    },
+    methods:{
+      onFileProgress(rootFile,file,chunk){
+        // console.log(rootFile);
+        // console.log(file.name);
+        // console.log(chunk);
+        // console.log('上传中 ${file.name}，chunk：${chunk.startByte / 1024 / 1024} ~ ${chunk.endByte / 1024 / 1024}');
+      },
+      onFileAdded(file){
+        // console.log(file);
+        // console.log('选择文件 ${file.name}');
+      },
+      onFileSuccess(rootFile,file,response,chunk){
+        // console.log(rootFile);
+        // console.log(file);
+        // console.log(response);
+        // console.log(chunk);
+        console.log(file.name + "upload success ");
+
+        console.log(response);
+      },
+      onFileError(rootFile,file,response,chunk){
+
+        console.log(file);
+      
+      }
+    }
+  }
+</script>
+
+<style>
+  .uploader-example {
+    width: 880px;
+    padding: 15px;
+    margin: 40px auto 0;
+    font-size: 12px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, .4);
+  }
+  .uploader-example .uploader-btn {
+    margin-right: 4px;
+  }
+  .uploader-example .uploader-list {
+    max-height: 440px;
+    overflow: auto;
+    overflow-x: hidden;
+    overflow-y: auto;
+  }
+</style>
+
---
+++ src/views/websocket/chat/components/card.vue
@@ -0,0 +1,66 @@
+<script>
+// import { actions } from '../store';
+
+export default {
+    vuex: {
+        // actions: actions,
+        getters: {
+            user: ({ user }) => user,
+            filterKey: ({ filterKey }) => filterKey
+        }
+    },
+    methods: {
+        onKeyup (e) {
+            // this.search(e.target.value);
+        }
+    }
+};
+</script>
+
+<template>
+<div class="card">
+    <header>
+        <img class="avatar" width="40" height="40" alt="user.name" src="https://dss0.bdstatic.com/6Ox1bjeh1BF3odCf/it/u=902229019,2472101033&fm=74&app=80&f=JPEG&size=f121,121?sec=1880279984&t=93f1bd18375537950a8fe0b67d0206cb">
+        <p class="name">用户名</p>
+    </header>
+    <footer>
+        <input class="search" type="text" placeholder="search user...">
+    </footer>
+
+</div>
+</template>
+
+<style scoped lang="less">
+.card {
+    padding: 12px;
+    border-bottom: solid 1px #24272C;
+
+    footer {
+        margin-top: 10px;
+    }
+
+    .avatar, .name {
+        vertical-align: middle;
+    }
+    .avatar {
+        border-radius: 2px;
+    }
+    .name {
+        display: inline-block;
+        margin: 0 0 0 15px;
+        font-size: 16px;
+    }
+    .search {
+        padding: 0 10px;
+        width: 100%;
+        font-size: 12px;
+        color: #fff;
+        height: 30px;
+        line-height: 30px;
+        border: solid 1px #3a3a3a;
+        border-radius: 4px;
+        outline: none;
+        background-color: #26292E;
+    }
+}
+</style>
